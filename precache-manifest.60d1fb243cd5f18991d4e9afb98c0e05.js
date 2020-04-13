255884d5c09895fbe28e88bbee1e12246ff98192
prashant kumar singh <singh.prashantking@gmail.com>
2020-04-14 00:22:49 +0530

    Updates

---
+++ precache-manifest.60d1fb243cd5f18991d4e9afb98c0e05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bea6c50d37529204bb8baa9cce9a078",
+    "url": "/typing-speed-calculator/index.html"
+  },
+  {
+    "revision": "7cf0a6b9934ec9a36ecf",
+    "url": "/typing-speed-calculator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c64b72f61196dbab7e26",
+    "url": "/typing-speed-calculator/static/js/2.71b73f73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/typing-speed-calculator/static/js/2.71b73f73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cf0a6b9934ec9a36ecf",
+    "url": "/typing-speed-calculator/static/js/main.0ff6b51f.chunk.js"
+  },
+  {
+    "revision": "4a0b66960821a02997a8",
+    "url": "/typing-speed-calculator/static/js/runtime-main.5bfe74d2.js"
+  }
+]);
